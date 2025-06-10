60edf3a7c19ac5bd8f181e8f8d32a7db6ea4ac4b
Nguyễn Hoàng Linh <93470801+nhlinhseuit@users.noreply.github.com>
2025-06-10 22:41:22 +0700

    first commit

---
+++ src/main/java/com/example/quizexercise/QuizExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.quizexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/quizexercise/QuizExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quizexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
