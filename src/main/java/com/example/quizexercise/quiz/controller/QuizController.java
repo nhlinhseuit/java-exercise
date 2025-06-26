dc3ccd0fb2dc27594e9bbff31f223edfc753e71e
Nguyễn Hoàng Linh <dev.hoanglinh@gmail.com>
2025-06-27 00:28:38 +0700

    feat: implement get quiz API

--- src/main/java/com/example/quizexercise/quiz/controller/QuizController.java
+++ src/main/java/com/example/quizexercise/quiz/controller/QuizController.java
@@ -5,6 +5,8 @@ import com.example.quizexercise.quiz.controller.dto.request.GetQuizListRequestDt
 import com.example.quizexercise.quiz.controller.dto.response.CreateQuizResponseDto;
 import com.example.quizexercise.quiz.controller.dto.response.GetQuizResponseDto;
 import com.example.quizexercise.quiz.service.QuizService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -28,5 +30,19 @@ public class QuizController {
   public CreateQuizResponseDto createQuiz(
       @RequestBody CreateQuizRequestDto createQuizListRequestDto) {
     return quizService.createQuiz(createQuizListRequestDto);
+  }
+
+  @GetMapping("/{quizId}")
+  @Operation(
+          summary = "Lấy thông tin bài quiz theo ID",
+          description = "Trả về nội dung bài quiz và danh sách câu hỏi kèm theo các phương án lựa chọn dựa trên ID của quiz."
+  )
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "200", description = "Lấy quiz thành công"),
+          @ApiResponse(responseCode = "404", description = "TEMP: Không tìm thấy quiz với ID được cung cấp"),
+          @ApiResponse(responseCode = "500", description = "TEMP: Lỗi hệ thống")
+  })
+  public GetQuizResponseDto getQuiz(@PathVariable("quizId") String quizId) {
+    return quizService.getQuiz(quizId);
   }
 }
--- src/main/java/com/example/quizexercise/quiz/controller/dto/response/GetQuizResponseDto.java
+++ src/main/java/com/example/quizexercise/quiz/controller/dto/response/GetQuizResponseDto.java
@@ -1,10 +1,12 @@
 package com.example.quizexercise.quiz.controller.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 import java.util.List;
 
 @Builder
+@Schema(description = "DTO phản hồi khi lấy một quiz")
 public record GetQuizResponseDto(
-    String content, List<QuestionResponseDto> questionList) {
-}
+    @Schema(description = "Nội dung của quiz", example = "Quiz về Java cơ bản") String content,
+    @Schema(description = "Danh sách câu hỏi thuộc quiz") List<QuestionResponseDto> questionList) {}
